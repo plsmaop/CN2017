510a61b90fb8dd3cd56fc6d45805fe38e289de6d
allenivan@gmail.com <allenivan@gmail.com>
2017-10-15 22:47:46 +0800

    beta

--- HW1/main.py
+++ HW1/main.py
@@ -2,7 +2,7 @@ import socket
 import os
 import string
 
-BOTName = "NICK ROBOT_tt"
+BOTName = "ROBOT_tt"
 IRCSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def pong(server_msg):
@@ -39,37 +39,70 @@ def recv_irc():
 
 def connect(Channel):
     IRCSocket.connect(("irc.freenode.net", 6667))
-    IRCSocket.send(BOTName+"\r\n")
+    IRCSocket.send("NICK " + BOTName + "\r\n")
     IRCSocket.send("USER ROBOT ROBOT bla :ROBOT\r\n")
     msg_recv = recv_irc()
     IRCSocket.send("JOIN " + Channel + "\r\n")
     IRCSocket.send("PRIVMSG " + Channel+ " :Hello! I am robot.\r\n")
 
-#def convert():
+def repeat(msg_recv):
+    repeat_start_pos = msg_recv.find("@repeat ") + 8
+    return msg_recv[repeat_start_pos:]
     
+def convert(msg_recv):
+    convert_number  = msg_recv[msg_recv.find("@convert ") + 9:]
+    try:
+        return hex(int(convert_number))
+    except ValueError:
+        try:
+            return str(int(convert_number, 16))
+        except ValueError:
+            return "Input_Error"
+
+def isValid(ip_string):
+    length = len(ip_string)
+    if length == 0 or length > 3 or (length > 1 and ip_string[0:1] == '0'):
+        return False
+    ip_number = int(ip_string)
+    return ip_number >= 0 and ip_number <= 255
+
+def ip_caculator(ip_string, segment_number, valid_ip, ip_list):
+    if segment_number == 0 and len(ip_string) == 0: ip_list.append(valid_ip)
+    else:
+        for i in range(1,4):
+            if len(ip_string) >= i and isValid(ip_string[:i]):
+                if segment_number == 1: ip_caculator(ip_string[i:], segment_number - 1, valid_ip + ip_string[:i], ip_list)
+                else: ip_caculator(ip_string[i:], segment_number - 1, valid_ip + ip_string[:i] + '.', ip_list)
+
+def ip(msg_recv):
+    ip_start_pos = msg_recv.find("@ip ") + 4
+    ip_string =  msg_recv[ip_start_pos:]
+    if len(ip_string) < 4:
+        print 0
+        return
+    ip_list = []
+    ip_caculator(ip_string, 4, '', ip_list)
+    return ip_list
+
     
-
-
 def reply(Channel, isInChatRoom = False):
     while True:
         msg_recv = recv_irc()
         if msg_recv.find("@ " + Channel) != -1:
             isInChatRoom = True
-        if isInChatRoom:
-            input_order = raw_input("Please enter something")
-            if input_order.find("c") != -1:
-                IRCSocket.close()
-                print "close the connect"
         
         if msg_recv.find("You have not registered") != -1:
             IRCSocket.close()
             break
         elif msg_recv.find("@repeat ") != -1:
-            IRCSocket.send("PRIVMSG " + Channel+ " :havn't finished\r\n")
+            IRCSocket.send("PRIVMSG " + Channel+ " " + repeat(msg_recv) +"\r\n")
         elif msg_recv.find("@convert ") != -1:
-            IRCSocket.send("PRIVMSG " + Channel+ " :havn't finished\r\n")
+            IRCSocket.send("PRIVMSG " + Channel+ " " + convert(msg_recv) + "\r\n")
         elif msg_recv.find("@ip ") != -1:
-            IRCSocket.send("PRIVMSG " + Channel+ " :havn't finished\r\n")
+            ip_list = ip(msg_recv)
+            IRCSocket.send("PRIVMSG " + Channel+ " " + len(ip_list) + "\r\n")
+            for i in ip_list:
+                IRCSocket.send("PRIVMSG " + Channel+ " " + i + "\r\n")
         elif msg_recv.find("@help") != -1:
             IRCSocket.send("PRIVMSG " + Channel+ " :@repeat <Message>\r\n")
             IRCSocket.send("PRIVMSG " + Channel+ " :@convert <Number>\r\n")
@@ -78,5 +111,3 @@ def reply(Channel, isInChatRoom = False):
 Channel = read_chan()
 connect(Channel)
 reply(Channel)
-
-
